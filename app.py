016360b8c253f17b0fb5283a4e720a09e85fb8a2
Jordy <54374139+JORDYgbg@users.noreply.github.com>
2026-04-29 19:10:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,18 +16,12 @@ def audio():
         return jsonify({'error': 'falta v'}), 400
     try:
         ydl_opts = {
-            'format': 'bestaudio[ext=m4a]/bestaudio/best',
+            'format': 'bestaudio/best',
             'quiet': True,
             'no_warnings': True,
             'cookiefile': COOKIES,
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-                'Accept-Language': 'es-EC,es;q=0.9',
-            },
-            'extractor_args': {
-                'youtube': {
-                    'player_client': ['web'],
-                }
             },
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
